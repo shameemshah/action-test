1ab8a70e3b101581ea08afa2aa74458ad654681e
Shameem <shameem.@keyvalue.systems>
2022-11-23 14:14:23 +0530

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #! /bin/bash 
-ReleaseNotes=LeloSch,jkame,KFFFkashi,Wrath
+ReleaseNotes=LeloSch,jkame,Kkashi,Wrath
 IFS=','
 read -ra arr <<< "$ReleaseNotes"
 for val in "${arr[@]}";
 do
-  ReleaseNotes=`printf " -  $val\n"`
+  ReleaseNotes=`printf " - $val\n"`
   echo $ReleaseNotes
 done
+
+foo="one two three"
+foo=( $foo ) # without quotes
+foo="${foo[@]^}"
+echo $foo
