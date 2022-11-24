40d603fc7e1b49ec482729c7497b5dea4cc942b9
Shameem <shameem.@keyvalue.systems>
2022-11-24 15:49:32 +0530

    test

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -1,11 +1,12 @@
 repo=shameemshah/action-test
-tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | jq -r '.tag_name')
-read type tag_sha < <(echo $(curl -s "https://api.github.com/repos/$repo/git/ref/tags/$tag" | 
+tag=$(curl -s -H "Authorization: Bearer $TOKEN"  "https://api.github.com/repos/$repo/releases/latest" | jq -r '.tag_name')
+read type tag_sha < <(echo $(curl -s -H "Authorization: Bearer $TOKEN" "https://api.github.com/repos/$repo/git/ref/tags/$tag" | 
      jq -r '.object.type,.object.sha'))
 
 if [ $type == "commit" ]; then
     echo "commit sha: $tag_sha"
 else
-    sha=$(curl -s "https://api.github.com/repos/$repo/git/tags/$tag_sha" | jq '.object.sha')
+    sha=$(curl -s -H "Authorization: Bearer $TOKEN" "https://api.github.com/repos/$repo/git/tags/$tag_sha" | jq '.object.sha')
     echo "commit sha: $sha"
 fi
+
